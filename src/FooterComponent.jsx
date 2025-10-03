55383f4bb6695b882d59d70c875320c4aa38abf5
HamzatAbdulaleem <abdulaleem.hamzat@codetrik.com.ng>
2025-10-03 12:46:35 +0100

    fix: remove inec details from footerComponent

--- src/FooterComponent.jsx
+++ src/FooterComponent.jsx
@@ -6,18 +6,6 @@ const FooterComponent = () => {
             <h4>Meet the team</h4>
             <section>
                 <div>
-                    <h5>INEC Officials</h5>
-                    <ul>
-                        <li>hamzat abdulaleem i.</li>
-                        <li>babarinde ayomide e.</li>
-                        <li>hamzat abdulaleem i.</li>
-                        <li>babarinde ayomide e.</li>
-                        <li>hamzat abdulaleem i.</li>
-                        <li>babarinde ayomide e.</li>
-                    </ul>
-                </div>
-
-                <div>
                     <h5>developers</h5>
                     <ul>
                         <li>hamzat abdulaleem i.</li>
